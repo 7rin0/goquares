91c43aeed23d00b262008bdab74a7d8e9ae63f8d
Luis SEVERINO <lfs.severino@gmail.com>
2016-04-09 09:50:01 +0200

    apply some settings to dev environment; mincss as grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      files: ['Gruntfile.js', 'src/js/*.js'],
       options: {
         globals: {
           jQuery: true
@@ -16,15 +16,26 @@ module.exports = function(grunt) {
         }
       }
     },
+    cssmin: {
+      min: {
+        files: {
+          'src/min/main.min.css': ['src/css/main.css']
+        }
+      }
+    },
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint']
     }
   });
 
+  // Load tasks
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['jshint', 'uglify']);
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Register tasks
+  grunt.registerTask('default', ['jshint', 'uglify', 'cssmin', 'watch']);
 
 };
