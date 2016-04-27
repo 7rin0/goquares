abd884b09409142a5a09334554723b8c60dcea96
Luis SEVERINO <lfs.severino@gmail.com>
2016-04-27 20:41:12 +0200

    test minified html

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,55 +1,67 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  grunt.initConfig({
-    jshint: {
-      files: ['Gruntfile.js', 'src/js/main.js'],
-      options: {
-        globals: {
-          jQuery: true
+    grunt.initConfig({
+        jshint: {
+            files: ['Gruntfile.js', 'src/js/main.js'],
+            options: {
+                globals: {
+                    jQuery: true
+                }
+            }
+        },
+        uglify: {
+            min: {
+                files: {
+                    'src/min/js/main.min.js': ['src/js/main.js']
+                }
+            }
+        },
+        cssmin: {
+            min: {
+                files: {
+                    'src/min/css/main.min.css': ['src/css/main.css']
+                }
+            }
+        },
+        htmlmin: {
+            min: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'index.html': 'src/html/index.html'
+                }
+            }
+        },
+        imagemin: {
+            dist: {
+                options: {
+                    optimizationLevel: 5
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/images',
+                    src: ['**/*.{png,jpg,gif,ico}'],
+                    dest: 'src/min/images'
+                }]
+            }
+        },
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint', 'uglify', 'cssmin', 'watch', 'htmlmin']
         }
-      }
-    },
-    uglify: {
-      min: {
-        files: {
-          'src/min/js/main.min.js': ['src/js/main.js']
-        }
-      }
-    },
-    cssmin: {
-      min: {
-        files: {
-          'src/min/css/main.min.css': ['src/css/main.css']
-        }
-      }
-    },
-    imagemin: {
-       dist: {
-          options: {
-            optimizationLevel: 5
-          },
-          files: [{
-             expand: true,
-             cwd: 'src/images',
-             src: ['**/*.{png,jpg,gif,ico}'],
-             dest: 'src/min/images'
-          }]
-       }
-    },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint', 'uglify', 'cssmin', 'watch']
-    }
-  });
+    });
 
-  // Load tasks
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    // Load tasks
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Register tasks
-  grunt.registerTask('default', ['jshint', 'uglify', 'cssmin', 'watch']);
+    // Register tasks
+    grunt.registerTask('default', ['jshint', 'uglify', 'cssmin', 'watch', 'htmlmin']);
 
 };
